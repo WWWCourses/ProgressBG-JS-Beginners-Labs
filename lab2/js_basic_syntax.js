107fead41942d876ee36ad6d0e30646f94bb6f67
WWWCourses <ivawebcourses@gmail.com>
2020-10-07 18:35:54 +0300

    Lab2: 07.10.2020 DONE: till https://wwwcourses.github.io/ProgressBG-JS-Beginners-Slides/pages/themes/beginners/JS_BasicSyntax_1/JS_BasicSyntax_1.html#/comparisonOperators NEXT: next

---
+++ lab2/js_basic_syntax.js
@@ -0,0 +1,89 @@
+// let _ = 9;
+// let x = 5;
+// let $ = 2;
+// let x1 = 3;
+// // let 1_ = 5; // error
+// let _1_ = 3; // ok
+// let вар = 4; // ok
+
+// let class = 4; // error
+
+// let un = 'Ada';
+// let userfirstname = 'Ada'; // not readable
+// let userFirstName = 'Ada'; // perfect
+
+// let x = 5;
+// x = 3.14;
+
+// let x = 2e3; // 2*10**3
+// console.log( x );
+
+// let debit = 1_000_000;
+// console.log( debit );
+
+// 3.14 //float
+// console.log( 0.1 + 0.2 ); // 0.3
+
+// let userName = 'ada byron';
+// let str = "John's pub";
+// console.log( str );
+
+// let str = 'a demo \' double: \" ';
+// console.log( str );
+// a demo ' double: "
+
+// console.log( '\i' );
+// console.log( '->>>\n<---' );
+
+// console.log( 'a\\b' );
+// a\b
+
+// let poem = "\tline 1\n\tline 2";
+// console.log( poem );
+
+// let poem = "line 1" +
+// "line2" +
+// "line3";
+// console.log( poem );
+
+// let userFirstName = 'Ada';
+// let userSurName = 'Byron';
+// before ES6:
+// console.log( userFirstName + ' ' + userSurName ); 'Ada Byron'
+// // after ES6:
+// console.log(`${userFirstName} ${userSurName}`);
+
+// console.log( user);
+// 2+4;
+// 'Ada';
+// userFirstName; // 'Ada'
+
+// console.log( "2+4"); // '2+4'
+// console.log(`2+4`);  // '2+4'
+// console.log(`${2+4}`);  // '2+4'
+
+// boolean
+// let x = 5;
+// let y = 'true';
+// let login = true; // bool
+// if( login ){
+// 	console.log('Welcome');
+// }
+
+// 5
+// '5';
+// let x = null; // null
+// let y = undefined; // undefined
+
+// let x = null;
+// console.log( typeof x );
+
+
+// console.log( typeof('d' / 2) ); //
+// console.log( typeof('d' / 2) ); //
+// console.log( typeof('d' / 2) ); //
+// console.log( typeof('d' / 2) ); //
+// console.log( typeof('d' / 2) ); //
+// console.log( typeof('d' / 2) ); //
+// console.log( typeof('d' / 2) ); //
+
---
+++ lab2/operators.js
@@ -0,0 +1,40 @@
+// console.log( 3%2 ); //1
+// console.log( 4%2 ); //0
+// console.log( 5%2 ); //1
+
+let a = 1;
+let b = 1;
+// a = a + 1;
+
+// console.log( a++ );// a++ == a   => 1
+// console.log( ++a );// ++a == a+1 => 3
+// console.log( a );  // 3
+
+// console.log( a++ + ++b ); // 1 + 2  =  3
+
+// let x = 3.1416;
+// console.log(`${Math.round(x)}`);
+// TASK: да се принтира закръглената стойност на x
+// console.log( ); // 3
+
+// let x = 4;
+// let x;
+// console.log( x = 4 );
+// let x = y = 1;
+// console.log(x,y);
+
+// 2 = 3; // error
+// let x;
+// x = 1;
+// console.log( x = 2 ); //
+
+// let x = 1;
+// x++; // x = x + 1
+// ++x; // x = x + 1
+// x+=1;// x = x + 1
+
+// console.log( {} * [] );
+
+console.log( 2 + '2'); // '22'
+console.log( 30 * '2'); // 60
+
