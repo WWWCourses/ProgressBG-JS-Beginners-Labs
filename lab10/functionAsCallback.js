a4e98d62bd405a087fd8c3589b2dbcff52286e87
WWWCourses <ivawebcourses@gmail.com>
2020-11-04 18:08:56 +0200

    Lab10: 04.11.2020 DONE: Event Object Intro NEXT: Event Object target use case

---
+++ lab10/functionAsCallback.js
@@ -0,0 +1,20 @@
+function writeInOutput() {
+	console.log(`1`);
+}
+
+let btnClick = {
+	"addEventListener": function (type, listener) {
+		console.log(`2. ${typeof listener}`);
+		if( type === 'click'){
+			listener();
+		}
+	}
+}
+
+
+// btnClick.addEventListener('click', writeInOutput() );
+btnClick.addEventListener('click', writeInOutput );
+
+
+// 2 Function
+// 1.
